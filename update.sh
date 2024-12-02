aa851b7ea97aa5d20baf0e79400dedce32b99283
Polymere <paul@biped.ai>
2024-12-02 17:25:37 +0100

    pin metrical to 11.0

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 git pull;
 docker pull bipedrobotics/camera_tools:latest;
-docker pull tangramvision/cli:latest;
+docker pull tangramvision/cli:11.0.0;
 pip install -r requirements.txt
 git submodule init
 git submodule update
