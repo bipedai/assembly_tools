4344bb97a6f80e6bc71dfc9586cbe528ae0ef380
Polymere <paul@biped.ai>
2025-01-23 15:00:13 +0100

    :hammer: Fix display for scan tool

--- scan_noa_on_network.sh
+++ scan_noa_on_network.sh
@@ -3,5 +3,5 @@
 network_sub=$(hostname -I | awk '{print $1}' | cut -d '.' -f 1-3)
 candidates=$(nmap -Pn --min-rate 1000 -p 22 -T 5 $network_sub.0-255 --open | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
 
-echo "Try connecting to one of the following IPs: \n$candidates \n with ssh khadas@<IP>"
+echo -e "Try connecting to one of the following IPs:\n$(echo "$candidates" | tr ' ' '\n')\nwith ssh khadas@<IP>"
 
