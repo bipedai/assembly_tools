e20507764e0a4275cccc36f49ab70479258f8f8b
prevel <paul.prevel@protonmail.com>
2025-05-16 10:31:18 +0200

    pin

--- camera_calibrate.sh
+++ camera_calibrate.sh
@@ -10,52 +10,45 @@ CALIBRATION_FOLDER="/tmp/$CALIB_UUID"
 mkdir -p $CALIBRATION_FOLDER
 chown $(id -u):$(id -g) $CALIBRATION_FOLDER
 
-
-camera_tools()
-{
+camera_tools() {
     docker run -it --rm --user="$(id -u):$(id -g)" \
-    --privileged \
-    -v /dev:/dev \
-    -v $CALIBRATION_FOLDER:/out \
-    --device-cgroup-rule "c 81:* rmw" \
-    --device-cgroup-rule "c 189:* rmw" \
-    bipedrobotics/camera_tools:latest \
-    -- "$@";
+        --privileged \
+        -v /dev:/dev \
+        -v $CALIBRATION_FOLDER:/out \
+        --device-cgroup-rule "c 81:* rmw" \
+        --device-cgroup-rule "c 189:* rmw" \
+        bipedrobotics/camera_tools:0.2.0 \
+        -- "$@"
 }
 
 metrical() {
     docker run --rm --init --user="$(id -u):$(id -g)" \
-    --volume="$CALIBRATION_FOLDER:/$CALIB_UUID" \
-    --workdir="/$CALIB_UUID" \
-    --net=host \
-    tangramvision/cli:11.0.0 \
-    --license="key/$TANGRAM_KEY" \
-	  "$@";
+        --volume="$CALIBRATION_FOLDER:/$CALIB_UUID" \
+        --workdir="/$CALIB_UUID" \
+        --net=host \
+        tangramvision/cli:11.0.0 \
+        --license="key/$TANGRAM_KEY" \
+        "$@"
 }
 
-
-
 camera_tools --mode calibrate -o /out/$CALIB_UUID
-
 
 cp $ASSEMBLY_TOOL_DIR/assets/$OBJ $CALIBRATION_FOLDER
 _INIT_PLEX_FILE="tmp_plex.json"
 _UMBRA_OUTPUT="umbra_$CALIB_UUID.json"
 
-
 metrical init \
-     --topic-to-model *:opencv_radtan \
-     --preset-device RealSense435:[cam_right_left_ir,cam_right_right_ir,_] \
-     --preset-device RealSense435:[cam_middle_left_ir,cam_middle_right_ir,_] \
-     --preset-device RealSense435:[cam_left_left_ir,cam_left_right_ir,_] \
-     $CALIB_UUID $_INIT_PLEX_FILE
-
+    --topic-to-model *:opencv_radtan \
+    --preset-device RealSense435:[cam_right_left_ir,cam_right_right_ir,_] \
+    --preset-device RealSense435:[cam_middle_left_ir,cam_middle_right_ir,_] \
+    --preset-device RealSense435:[cam_left_left_ir,cam_left_right_ir,_] \
+    $CALIB_UUID $_INIT_PLEX_FILE
 
 echo "Computing calibration"
-metrical calibrate --interactive --disable-filter  $CALIB_UUID $_INIT_PLEX_FILE $OBJ --output-json $_UMBRA_OUTPUT
+metrical calibrate --interactive --disable-filter $CALIB_UUID $_INIT_PLEX_FILE $OBJ --output-json $_UMBRA_OUTPUT
 
 echo "Generating the calibration table"
-jq .plex "$CALIBRATION_FOLDER/$_UMBRA_OUTPUT" > "$CALIBRATION_FOLDER/calib_table.json"
+jq .plex "$CALIBRATION_FOLDER/$_UMBRA_OUTPUT" >"$CALIBRATION_FOLDER/calib_table.json"
 mv $CALIBRATION_FOLDER/$CALIB_UUID/camera_position_to_camera_serial.json $CALIBRATION_FOLDER
 
 camera_tools --mode flash_calibration -i /out
